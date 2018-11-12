64de0756fe4c57266fdeda8c366e16343b44b1dc
Natalie Myers <nataliehmyers@gmail.com>
2018-11-12 08:59:26 -0600

    Exercise with memory allocation and strings

--- InClassLabs/a-lab10.c
+++ InClassLabs/a-lab10.c
@@ -102,7 +102,7 @@ int checkString(char *pointer, int size) {
 void cipherString(char *pointer, int size, int shift) {
     for (int i = 0; i < size; i++) {
         *(pointer + i) = (*(pointer + i) + shift);
-            while (shift + i > 90) {
+            if (shift + i > 90) {
                 *(pointer + i) = (*(pointer + i) - 26 + shift);
         }
     }
