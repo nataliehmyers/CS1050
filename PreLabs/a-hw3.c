74bce3650487a2a368e2fdf3fcd9897527da58ab
Natalie Myers <nataliehmyers@gmail.com>
2018-11-23 17:58:48 -0800

    An exercise in file I/O processing

--- PreLabs/a-hw3.c
+++ PreLabs/a-hw3.c
@@ -9,5 +9,16 @@ int lowest_amount(float*, int);
 float average_amount(float*, int);
 int write_data(char*, int*, float *, int , int, int, float);
 
-void main(argc) {
+int main(int argc, char *filename[]) {
+    int size;
+    if (argc < 2) { // checks for appropriate number of command line arguments
+        printf("Incorrect number of arguments\nSyntax: ./a.out input_file_name output_file_name\n");
+        return 1;
+    }
+    FILE *fp;
+    fp = fopen(filename, "r");
+    for (int i = 0; i < size; i++) {
+        //
+    }
+    return 0;
 }
