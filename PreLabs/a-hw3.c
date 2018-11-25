14c28a637807c753442307433bc9175817120d10
Natalie Myers <nataliehmyers@gmail.com>
2018-11-24 17:19:44 -0800

    An exercise in file I/O processing

--- PreLabs/a-hw3.c
+++ PreLabs/a-hw3.c
@@ -12,21 +12,26 @@ int lowest_amount(float *, int);
 float average_amount(float *, int);
 int write_data(char *, int *, float *, int , int, int, float);
 
-int main(int argc, char *filename[]) {
-    if (argc < 2) { // checks for appropriate number of command line arguments
+int main(int argc, char *argv[]) {
+    if (argc < 3) { // checks for appropriate number of command line arguments
         printf("Incorrect number of arguments\nSyntax: ./a.out input_file_name output_file_name\n");
         return 1;
     }
-    int record_count = get_record_count(*filename);
+    char *input_file = argv[1];
+    char *output_file = argv[2];
+    int record_count = get_record_count(input_file);
     int *accounts = malloc(sizeof(int) * record_count);
     float *balances = malloc(sizeof(float) * record_count);
 
-    printf("\nLines: %d\n\n", record_count);
-
-    load_data(*filename, accounts, balances, record_count);
+    load_data(input_file, accounts, balances, record_count);
     print_data(accounts, balances, record_count);
-
-
+    int max_index = highest_amount(balances, record_count);
+    printf("\nThe highest amount of $%.2f is in the account number %d", balances[max_index], accounts[max_index]);
+    int min_index = lowest_amount(balances, record_count);
+    printf("\nThe lowest amount of $%.2f is in the account number %d", balances[min_index], accounts[min_index]);
+    float calc_average = average_amount(balances, record_count);
+    printf("\nThe average amount is $%.2f\n", calc_average);
+    write_data(output_file, accounts, balances, record_count, max_index, min_index, calc_average);
 
     free(accounts);
     free(balances);
@@ -37,14 +42,16 @@ int get_record_count(char *filename) {
     int next_char;
     FILE *fp; // defines a FILE pointer
     fp = fopen(filename, "r"); // opens a file in read mode and sets the command to a variable
-    while(!feof(fp))
+    next_char = fgetc(fp);
+    while(next_char != EOF)
     {
-        next_char = fgetc(fp);
         if(next_char == '\n')
         {
             num_count++;
         }
+        next_char = fgetc(fp);
     }
+    fclose(fp);
     return num_count;
 }
 
@@ -66,4 +73,51 @@ void print_data(int *accounts, float *balances, int record_count){
     for(int i = 0; i < record_count; i++) {
         printf("%d\t\t%.2f\n", accounts[i], balances[i]);
     }
+}
+
+int highest_amount(float *balances, int record_count) {
+    float max = -2147483648;
+    int index = -1;
+    for (int i = 0; i < record_count; i++) {
+        if (balances[i] > max) {
+            max = balances[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+int lowest_amount(float *balances, int record_count) {
+    float min = 2147483647;
+    int index = -1;
+    for (int i = 0; i < record_count; i++) {
+        if (balances[i] < min) {
+            min = balances[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+float average_amount(float *balances, int record_count) {
+    float total = 0;
+    for (int i = 0; i < record_count; i++) {
+        total += balances[i];
+    }
+    return total/record_count;
+}
+
+int write_data(char *output_file, int *accounts, float *balances, int record_count, int max_index, int min_index, float calc_average) {
+    FILE *fp; // defines a FILE pointer
+    fp = fopen(output_file, "w");
+    fprintf(fp, "Account No.\tAmount\n");
+    for (int i = 0; i < record_count; i++) {
+        fprintf(fp, "%d\t\t%.2f\n", accounts[i], balances[i]);
+    }
+    fprintf(fp, "\nhe highest amount of $%.2f is in the account number %d"
+                "\nThe lowest amount of $%.2f is in the account number %d"
+                "\nThe average amount is $%.2f\n",
+            balances[max_index], accounts[max_index],
+            balances[min_index], accounts[min_index],
+            calc_average);
 }
--- PreLabs/placeholder.c
+++ PreLabs/placeholder.c
@@ -1,123 +1,37 @@
-/* a-prelab10.c - Uses malloc function to allocate space (to store the string)
- * instead of creating fixed size character array */
-// Created by Natalie Myers on 11/4/18
+// Program that uses command line arguments to read file names, memory allocation, and FILE pointers
+// Created by Natalie Myers on 11/13/18
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#define MAX 20
 
-int getString(char *);
-int checkString(char *, int);
-int errorCheck(int);
-void string_length(char *, int);
-void invalid_string_message(char *, int);
-void error_check_cont(int);
-// void getSafeString(char *, int);
+int get_record_count(char *);
+int load_data(char *, int *, float *, int);
+void print_data(int *, float *, int);
+int highest_amount(float *, int);
+int lowest_amount(float *, int);
+float average_amount(float *, int);
+int write_data(char *, int *, float *, int , int, int, float);
 
-int main(void) {
-    int size;
-    char *pointer = malloc(sizeof(char) * MAX);
-
-    size = getString(pointer);
-    printf("\nYou entered: %s which is size %d\n", pointer, size);
+int main(int argc, char *filename[]) {
+    if (argc < 2) { // checks for appropriate number of command line arguments
+        printf("Incorrect number of arguments\nSyntax: ./a.out input_file_name output_file_name\n");
+        return 1;
+    }
+    int record_count = get_record_count(*filename);
+    printf("Lines: %d\n", record_count);
 }
 
-int getString(char *pointer) {
-    int size;
-
-    printf("Enter the size of the string: ");
-    scanf("%d", &size);
-
-
-    error_check_cont(size);
-    printf("\nPlease enter the string: ");
-    scanf("%20s", pointer);
-
-
-    string_length(pointer, size);
-
-
-    invalid_string_message(pointer, size);
-
-
-
-    size = strlen(pointer);
-    return size;
-}
-
-int errorCheck(int size) {
-    if (size < 1 || size > 20) {
-        return 0;
-    }
-    return 1;
-}
-
-int checkString(char *pointer, int size) {
-    int i;
-    for (int i = 0; i < size; i++) {
-        *(pointer + i) = toupper(*(pointer + i));
-    }
-    for (i = 0; i < size; i++) {
-        if ((*(pointer + i) < 65 || *(pointer + i) > 90) && *(pointer + i) != '\0') {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-
-void string_length(char *pointer, int size) {
-    while (strlen(pointer) > size) {
-        printf("\nThe string entered is longer than the allowed size\n"
-               "\nPlease enter a valid string: ");
-        scanf("%20s", pointer);
-        invalid_string_message(pointer, size);
-    }
-}
-
-
-void invalid_string_message(char *pointer, int size) {
-    while (checkString(pointer, size) == 0) {
-        printf("\nPlease enter a valid string: ");
-        scanf("%20s", pointer);
-        string_length(pointer, size);
-    }
-}
-
-void error_check_cont(int size) {
-    while (errorCheck(size) == 0) {
-        printf("\nPlease enter again: ");
-        scanf("%d", &size);
-    }
-}
-
-
-
-/*
-void getSafeString(char *pointer, int maxsize) {
-
-    int indexcur = 0;
-    char charcur = '\n';
-
-    while ('\n'==charcur){
-        charcur = getc(stdin);
-    }
-    while (indexcur<(maxsize-1) && '\n'!=charcur)
-    {
-        if ('\n'!=charcur)
+int get_record_count(char *filename){
+    int num_count = 1;
+    int next_char;
+    FILE *fp; // defines a FILE pointer
+    fp = fopen(filename, "r"); // opens a file in read mode and sets the command to a variable
+    while(!feof(fp)) {
+        next_char = fgetc(fp);
+        if(next_char == '\n')
         {
-            pointer[indexcur] = charcur;
+            num_count++;
         }
-        indexcur++;
-        charcur = getc(stdin);
     }
-    pointer[indexcur] = '\0';
-
-    while ('\n'!=charcur)
-    {
-        charcur = getc(stdin);
-    }
+    return num_count;
 }
- */
